9fbc48615e9d614a2a1928341e2bb6c1e40fc3a3
vivianluomin <273104241@qq.com>
2018-07-07 23:39:31 +0800

    简单场景制作完成

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+    public float rotSpeed = 1.5f;
+    private float rotY;
+    private Vector3 offest;
+
+	// Use this for initialization
+	void Start () {
+        rotY = transform.eulerAngles.y;
+        offest = target.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+       
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movment = Vector3.zero;
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+
+        if(horInput!= 0 || vertInput != 0)
+        {
+            movment.z = horInput;
+            movment.x = vertInput;
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movment = target.TransformDirection(movment);
+            target.rotation = tmp;
+
+            transform.rotation = Quaternion.LookRotation(movment);
+        }
+	}
+}
