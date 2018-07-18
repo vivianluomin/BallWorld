c40ef420749a05ffef4f2552bb948eeac70bc4cd
vivianluomin <273104241@qq.com>
2018-07-18 18:59:54 +0800

    正式开始做

---
+++ Assets/Delegates/Scripts/Events/ObjectEvents.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+[AddComponentMenu("Delegates/Events/ObjectEvents")]
+public class ObjectEvents : MonoBehaviour
+{
+	[HideInInspector]
+	public Delegate onStart;
+	
+	[HideInInspector]
+	public Delegate onAwake;
+	
+	[HideInInspector]
+	public Delegate onEnable;
+	
+	[HideInInspector]
+	public Delegate onDisable;
+	
+	public void OnEnable() 	{onEnable.Exec(this);}
+	public void OnDisable() {onDisable.Exec(this);}
+	public void Start() 	{onStart.Exec(this);}
+	public void Awake() 	{onAwake.Exec(this);}
+}
